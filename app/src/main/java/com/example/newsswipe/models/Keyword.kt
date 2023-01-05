03e50157163d03a311e60ea159c95b38f167e425
Pierre-Cognard <pierre.cognard@alumni.univ-avignon.fr>
2023-01-05 17:21:21 +0100

    News opening

--- app/src/main/java/com/example/newsswipe/models/Keyword.kt
+++
@@ -1,4 +0,0 @@
-package com.example.newsswipe.models
-
-class Keyword {
-}
--- app/src/main/java/com/example/newsswipe/ui/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/newsswipe/ui/adapter/NewsAdapter.kt
@@ -6,13 +6,10 @@ import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.*
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.example.newsswipe.R
